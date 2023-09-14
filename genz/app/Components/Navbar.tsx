afe20442fa7f81128a5d9e6badf14fd9f47ff4fe
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-09-15 00:25:09 +0530

    started making genz

---
+++ genz/app/Components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import img from '../../ublicc/next.svg'
+import Image from 'next/image'
+
+const Navbar = () => {
+  return (
+    <div>
+      hellpoo
+      <Image src={img} alt="" />
+    </div>
+  )
+}
+
+export default Navbar
